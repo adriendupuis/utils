0daa70dc5618bc4bb1a2e32f3592ff1a672315ef
Adrien DUPUIS <adrien.dupuis@ez.no>
2020-03-31 15:40:54 +0200

    aliases.sh: Start a list of useful aliases

---
+++ aliases.sh
@@ -0,0 +1,4 @@
+alias git-graph='git log --all --graph --date-order --oneline --decorate';
+alias git-current-branch='git rev-parse HEAD --abbrev-ref';
+alias git-current-commit='git rev-parse HEAD --verify --short';
+alias git-clean-local-branches='echo TODO';
