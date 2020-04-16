a8f34aa57716b8d361cb357a4a5c20a4fb8a64bc
Adrien DUPUIS <adrien.dupuis@ez.no>
2020-04-16 14:46:41 +0200

    aliases.sh: Add really hard git reset

--- aliases.sh
+++ aliases.sh
@@ -1,4 +1,5 @@
 alias git-graph='git log --all --graph --date-order --oneline --decorate';
 alias git-current-branch='git rev-parse --abbrev-ref HEAD';
 alias git-current-commit='git rev-parse --verify --short HEAD';
+alias git-reset='git reset --hard origin/$(git-current-branch)';
 alias git-clean-local-branches='echo TODO';
