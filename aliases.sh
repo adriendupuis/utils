cd4dcc5c7f4e3d3618d621761cacf4a646f46df0
Adrien DUPUIS <adrien.dupuis@ez.no>
2020-04-28 19:05:56 +0200

    aliases.sh: git-current-tag: Force update from remote

--- aliases.sh
+++ aliases.sh
@@ -1,6 +1,6 @@
 alias git-graph='git log --all --graph --date-order --oneline --decorate';
 alias git-current-branch='git rev-parse --abbrev-ref HEAD';
 alias git-current-commit='git rev-parse --verify --short HEAD';
-alias git-current-tag='git fetch --tags && git describe --tags';
+alias git-current-tag='git fetch --tags --force && git describe --tags';
 alias git-reset='git reset --hard origin/$(git-current-branch)';
 alias git-clean-local-branches='echo TODO';
