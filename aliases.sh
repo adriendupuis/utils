40fbb33920130edb9d52f641aef702d70500e3eb
Adrien DUPUIS <adrien.dupuis@ez.no>
2020-05-12 18:27:17 +0200

    aliases.sh: Quiet git-current-tag
    - When there isn't any tag, no "fatal: No names found, cannot describe anything."
    - Quiet remote tag fetch

--- aliases.sh
+++ aliases.sh
@@ -1,6 +1,6 @@
 alias git-graph='git log --all --graph --date-order --oneline --decorate';
 alias git-current-branch='git rev-parse --abbrev-ref HEAD';
 alias git-current-commit='git rev-parse --verify --short HEAD';
-alias git-current-tag='git fetch --tags --force && git describe --tags';
+alias git-current-tag='git fetch --tags --force --quiet && git describe --tags 2> /dev/null';
 alias git-reset='git reset --hard origin/$(git-current-branch)';
 alias git-clean-local-branches='git fetch --quiet && for branch in $(git branch -vv | grep -v  "^\*" | grep "\[.*: gone\]" | cut -d " " -f 3); do git branch --delete "$branch"; done'
