fd062dc05f7f3bc454eae64f54c136c177482368
Adrien DUPUIS <adrien.dupuis@ez.no>
2020-05-15 12:02:43 +0200

    Continue utils-update

--- README.md
+++ README.md
@@ -8,9 +8,12 @@ Aliases
 
 ### Install
 
+Utils must be installed in ~/Utils.
+
 1. `git clone https://github.com/adriendupuis/utils.git -b master ~/utils;`
 1. Add `source $HOME/utils/aliases.sh` to your .zshrc or .bashrc
 
 ### Usage
 
-`utils-update`: Update utils
+- `utils-update`: Update utils
+- `utils-install <version>`: Install a version of utils, the version can be a tag or a branch
--- functions.sh
+++ functions.sh
@@ -1,8 +1,34 @@
 function _utils_update {
   cd ~/utils;
+
+  previous=$(_utils_version);
+
   git fetch;
   if [[ 0 -lt $# ]]; then
     git checkout "$1";
+  else
+    git checkout master;
   fi;
   git pull;
+
+  current=$(_utils_version);
+
+  if [ "$current" == "$previous" ]; then
+    echo "Utils is up-to-date and actualy in $previous";
+  else
+    source ~/utils/aliases.sh;
+    echo "Utils updated from $previous to $current.";
+  fi
+}
+
+function _utils_version {
+  cd ~/utils;
+
+  git fetch --tags --quiet;
+  version=$(git describe --tags 2> /dev/null);
+  if [ -z "$version" ]; then
+    version=$(git rev-parse --verify --short HEAD);
+  fi;
+
+  echo "$version";
 }
