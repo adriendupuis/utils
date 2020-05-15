10795ba39dc60a0bdbcd1595df18bcf90bcfa26b
Adrien DUPUIS <adrien.dupuis@ez.no>
2020-05-15 11:09:01 +0200

    aliases.sh: Rewrite to make `git-clean-local-branches --force` possible

--- aliases.sh
+++ aliases.sh
@@ -3,4 +3,4 @@ alias git-current-branch='git rev-parse --abbrev-ref HEAD';
 alias git-current-commit='git rev-parse --verify --short HEAD';
 alias git-current-tag='git fetch --tags --force --quiet && git describe --tags 2> /dev/null';
 alias git-reset='git reset --hard origin/$(git-current-branch)';
-alias git-clean-local-branches='git fetch --quiet && for branch in $(git branch -vv | grep -v  "^\*" | grep "\[.*: gone\]" | cut -d " " -f 3); do git branch --delete "$branch"; done';
+alias git-clean-local-branches='git fetch --quiet && git branch -vv | grep -v  "^\*" | grep "\[.*: gone\]" | cut -d " " -f 3 | xargs git branch --delete';
