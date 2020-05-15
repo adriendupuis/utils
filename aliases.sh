61368015aff96f356bc800d7a483dc27ef637a76
Adrien DUPUIS <adrien.dupuis@ez.no>
2020-05-15 12:03:06 +0200

    Continue utils-update

--- aliases.sh
+++ aliases.sh
@@ -2,7 +2,7 @@ source ~/utils/functions.sh;
 
 alias utils-install='utils-update';
 alias utils-update='_utils_update';
-alias utils-version='cd ~/utils && git-current-tag'
+alias utils-version='_utils_version'
 
 alias git-graph='git log --all --graph --date-order --oneline --decorate';
 alias git-current-branch='git rev-parse --abbrev-ref HEAD';
