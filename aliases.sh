8ee589ac59c5f2aa5cc2fb35d2608204d066d915
Adrien DUPUIS <adrien.dupuis@ez.no>
2020-05-27 18:24:58 +0200

    aliases.sh: Add ls alias

--- aliases.sh
+++ aliases.sh
@@ -4,3 +4,10 @@ alias git-current-commit='git rev-parse --verify --short HEAD';
 alias git-current-tag='git fetch --tags --force --quiet && git describe --tags 2> /dev/null';
 alias git-reset='git reset --hard origin/$(git-current-branch)';
 alias git-clean-local-branches='git fetch --quiet && git branch -vv | grep -v  "^\*" | grep "\[.*: gone\]" | cut -d " " -f 3 | xargs git branch --delete';
+
+if [ 'Darwin' == `uname` ]; then
+  # Mac OS X
+  alias ls='ls -GhalF@';
+else
+  alias ls='ls -halF --color';
+fi;
