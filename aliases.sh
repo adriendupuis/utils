1f3304d8468452405b4d5c1fb4e28c65ebce848c
Adrien DUPUIS <adrien.dupuis@ez.no>
2020-06-04 13:42:49 +0200

    Add package-version

--- aliases.sh
+++ aliases.sh
@@ -4,3 +4,5 @@ alias git-current-commit='git rev-parse --verify --short HEAD';
 alias git-current-tag='git fetch --tags --force --quiet && git describe --tags 2> /dev/null';
 alias git-reset='git reset --hard origin/$(git-current-branch)';
 alias git-clean-local-branches='git fetch --quiet && git branch -vv | grep -v  "^\*" | grep "\[.*: gone\]" | cut -d " " -f 3 | xargs git branch --delete';
+
+alias package-version='function _composer_package_version { grep "\"name\": \".*$1\"" -A 1 composer.lock; }; _composer_package_version';
