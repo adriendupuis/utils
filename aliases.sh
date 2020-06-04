f78046c35f31224d53ea6ad330ff763cabf9819b
Adrien DUPUIS <adrien.dupuis@ez.no>
2020-06-04 13:45:18 +0200

    package-version: Show source reference

--- aliases.sh
+++ aliases.sh
@@ -5,4 +5,4 @@ alias git-current-tag='git fetch --tags --force --quiet && git describe --tags 2
 alias git-reset='git reset --hard origin/$(git-current-branch)';
 alias git-clean-local-branches='git fetch --quiet && git branch -vv | grep -v  "^\*" | grep "\[.*: gone\]" | cut -d " " -f 3 | xargs git branch --delete';
 
-alias package-version='function _composer_package_version { grep "\"name\": \".*$1\"" -A 1 composer.lock; }; _composer_package_version';
+alias package-version='function _composer_package_version { grep "\"name\": \".*$1\"" -A 5 composer.lock; }; _composer_package_version';
