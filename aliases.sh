23ec10ec40a7790cb126acfb1df153d86d618597
Adrien DUPUIS <adrien.dupuis@ez.no>
2020-07-08 18:57:22 +0200

    Add `sf-cs-fixer` alias (PHP CS Fixer w/ Symfony rules)

--- aliases.sh
+++ aliases.sh
@@ -10,3 +10,5 @@ alias git-current-commit='git rev-parse --verify --short HEAD';
 alias git-current-tag='git fetch --tags --force --quiet && git describe --tags 2> /dev/null';
 alias git-reset='git reset --hard origin/$(git-current-branch)';
 alias git-clean-local-branches='git fetch --quiet && git branch -vv | grep -v  "^\*" | grep "\[.*: gone\]" | cut -d " " -f 3 | xargs git branch --delete';
+
+alias sf-cs-fixer='php-cs-fixer fix --rules=@Symfony';
