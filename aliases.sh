ae4a22c123b56c46af4a07a7a531423281a51b09
Adrien Dupuis <61695653+adriendupuis@users.noreply.github.com>
2020-09-09 11:48:42 +0200

    sf-cs-fixer → sf-cs-fix

--- aliases.sh
+++ aliases.sh
@@ -11,4 +11,4 @@ alias git-current-tag='git fetch --tags --force --quiet && git describe --tags 2
 alias git-reset='git reset --hard origin/$(git-current-branch)';
 alias git-clean-local-branches='git fetch --quiet && git branch -vv | grep -v  "^\*" | grep "\[.*: gone\]" | cut -d " " -f 3 | xargs git branch --delete';
 
-alias sf-cs-fixer='php-cs-fixer fix --rules=@Symfony';
+alias sf-cs-fix='php-cs-fixer fix --rules=@Symfony';
