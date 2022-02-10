1174c531f152ece04b5b96dc7732b795ce53ccb3
Adrien Dupuis <adrien.dupuis@ibexa.co>
2022-02-10 11:55:46 +0100

    `cpd`: Create a directory and copy files to it. `mvd`…

--- aliases.sh
+++ aliases.sh
@@ -9,13 +9,16 @@ alias git-current-branch='git rev-parse --abbrev-ref HEAD';
 alias git-current-commit='git rev-parse --verify --short HEAD';
 alias git-current-tag='git fetch --tags --force --quiet && git describe --tags 2> /dev/null';
 alias git-reset='git reset --hard origin/$(git-current-branch)';
+alias git-clean='git clean -df';
 alias git-clean-local-branches='git fetch --prune --quiet && git branch -vv | grep -v  "^\*" | grep "\[.*: gone\]" | cut -d " " -f 3 | xargs git branch --delete';
 alias git-force-pull='git fetch --force && git pull --force';
 alias git-global-ignore='git config --global core.excludesfile "~/utils/gitignore"';
 
 alias sf-cs-fix='php-cs-fixer fix --rules=@Symfony';
 
-alias mv-ln='function _mv_ln { mv $1 $2 && ln -s $2 $1 }; _mv_ln';
+alias mv-ln='function _mv_ln { mv $1 $2 && ln -s $2 $1; }; _mv_ln';
+alias cpd='function _cpd { mkdir -p ${@: -1} && cp $@; }; _cpd';
+alias mvd='function _mvd { mkdir -p ${@: -1} && mv $@; }; _mvd';
 alias gz='gzip --keep';
 
 if [ 'Darwin' = `uname` ]; then
