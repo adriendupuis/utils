deb5c27a8a251c2d80a9dc55dae35902ca0cdada
Adrien Dupuis <adrien.dupuis@ibexa.co>
2022-03-29 11:19:06 +0200

    aliases.sh: Add git-tag-list

--- aliases.sh
+++ aliases.sh
@@ -8,6 +8,7 @@ alias git-graph='git log --all --graph --date-order --oneline --decorate';
 alias git-current-branch='git rev-parse --abbrev-ref HEAD';
 alias git-current-commit='git rev-parse --verify --short HEAD';
 alias git-current-tag='git fetch --tags --force --quiet && git describe --tags 2> /dev/null';
+alias git-tag-list='git tag --sort=-committerdate --format "%(refname:short)%09%(committerdate:short)"';
 alias git-reset='git reset --hard origin/$(git-current-branch)';
 alias git-clean-local-branches='git fetch --prune --quiet && git branch -vv | grep -v  "^\*" | grep "\[.*: gone\]" | cut -d " " -f 3 | xargs git branch --delete';
 alias git-force-pull='git fetch --force && git pull --force';
