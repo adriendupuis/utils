8a92714529cc309dccbe60097ed57b47fce233db
Adrien Dupuis <61695653+adriendupuis@users.noreply.github.com>
2025-06-16 15:35:56 +0200

    aliases.sh: Fix git-clean-local-branches vs l10n
    Translation of "gone" into another language was able to break the alias.
    Force language to en_GB to ensure "gone" is the keyword to look for.

--- aliases.sh
+++ aliases.sh
@@ -10,7 +10,7 @@ alias git-current-commit='git rev-parse --verify --short HEAD';
 alias git-current-tag='git fetch --tags --force --quiet && git describe --tags 2> /dev/null';
 alias git-tag-list='git fetch --tags --force --quiet && git tag --sort=-committerdate --format "%(refname:short)%09%(committerdate:short)%09%(objectname:short=8)"';
 alias git-reset='git reset --hard origin/$(git-current-branch)';
-alias git-clean-local-branches='git fetch --prune --quiet && git branch -vv | grep -v  "^\*" | grep "\[.*: gone\]" | cut -d " " -f 3 | xargs git branch --delete';
+alias git-clean-local-branches='git fetch --prune --quiet && LANG=en_GB git branch -vv | grep -v  "^\*" | grep "\[.*: gone\]" | cut -d " " -f 3 | xargs git branch --delete';
 alias git-force-pull='git fetch --force && git pull --force';
 alias git-delete-tag='_git_delete_tag';
 alias git-global-ignore='git config --global core.excludesfile "~/utils/gitignore"';
